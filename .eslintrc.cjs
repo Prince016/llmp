5659d4bc1fab21fd8b800e37fcc157672f3e1440
Prince016 <prashantsharma3854@gmail.com>
2026-05-03 13:30:11 +0530

    first

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": "warn"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className=" bg-black text-white">this is react</div>
+    </>
+  );
+};
+
+export default App;
