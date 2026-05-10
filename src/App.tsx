036541adaa16532a19e7406f091f2f74620fb693
Prince016 <prashantsharma3854@gmail.com>
2026-05-10 17:03:09 +0530

    fix the dependency issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material';
 import './App.css';
 
 const App = () => {
   return (
     <>
       <div className=" bg-black text-white">this is react asthis di teh main thing</div>
+      <Button variant="contained">Hello, World!</Button>
     </>
   );
 };
